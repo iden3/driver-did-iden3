7a002472633989046acd16e9960b069f1c21901e
ilya-korotya <officalbatya@gmail.com>
2022-02-08 17:37:42 +0200

    return full did document

--- pkg/app/handler.go
+++ pkg/app/handler.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/iden3/driver-did-iden3/pkg/services"
-	core "github.com/iden3/go-iden3-core"
 )
 
 type DidDocumentHandler struct {
@@ -17,23 +16,13 @@ type DidDocumentHandler struct {
 // Get a did document by a did identifier.
 func (d *DidDocumentHandler) Get(w http.ResponseWriter, r *http.Request) {
 	rawURL := strings.Split(r.URL.Path, "/")
-	did, err := core.ParseDID(rawURL[len(rawURL)-1])
-	if err != nil {
-		log.Println("invalid did in request:", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
 
-	log.Println("did from request:", did)
-
-	state, err := d.DidDocumentService.GetDidDocument(r.Context(), did.ID)
+	state, err := d.DidDocumentService.GetDidDocument(r.Context(), rawURL[len(rawURL)-1])
 	if err != nil {
 		log.Printf("invalid get did document: %+v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	log.Println("state from smart contract:", state)
 
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -52,8 +41,6 @@ func (d *DidDocumentHandler) GetByDNSDomain(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	log.Println("state from smart contract:", state)
-
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(state)
@@ -69,8 +56,6 @@ func (d *DidDocumentHandler) GetByENSDomain(w http.ResponseWriter, r *http.Reque
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	log.Println("state from smart contract:", state)
 
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
