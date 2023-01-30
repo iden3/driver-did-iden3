5750d8a9acd721d4ffe29428a8d9eca43d82247f
ilya-korotya <officalbatya@gmail.com>
2023-01-30 17:37:30 +0200

    add readiness probe

--- pkg/app/router.go
+++ pkg/app/router.go
@@ -1,6 +1,9 @@
 package app
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type Handlers struct {
 	DidDocumentHandler *DidDocumentHandler
@@ -13,6 +16,13 @@ func (s *Handlers) Routes() *http.ServeMux {
 	mux.HandleFunc("/1.0/gist", s.DidDocumentHandler.GetGist)
 	mux.HandleFunc("/dns/", s.DidDocumentHandler.GetByDNSDomain)
 	mux.HandleFunc("/ens/", s.DidDocumentHandler.GetByENSDomain)
+	mux.HandleFunc("/status", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(`{"status":"OK"}`))
+		if err != nil {
+			log.Println("failed send body response")
+		}
+	})
 
 	return mux
 }
